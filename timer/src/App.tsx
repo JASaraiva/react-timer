61dcef18653b19977e3c98e8e66856b4c6eae916
Jonas A. saraiva <jonas.a.saraiva@accenture.com>
2022-08-20 09:23:56 -0300

    add rotas

--- timer/src/App.tsx
+++ timer/src/App.tsx
@@ -1,5 +1,14 @@
+import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
+import { defautTheme } from './styles/themes/default'
+import { BrowserRouter } from 'react-router-dom'
+
 export function App() {
   return (
-  <div>APP</div>
+    <ThemeProvider theme={defautTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
