b9454c53677da538a25e00b49dac4911d536fe4e
Jonas A. saraiva <jonas.a.saraiva@accenture.com>
2022-08-23 11:59:29 -0300

    estilizacao home

---
+++ timer/src/pages/home/index.tsx
@@ -0,0 +1,35 @@
+import { Play } from 'phosphor-react'
+import {
+  CountDownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+  StartCountDownButton,
+} from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em </label>
+          <input name="task" />
+          <label htmlFor="minutesAmount">durante </label>
+          <input name="minutesAmount" />
+          <span>minutos.</span>
+        </FormContainer>
+        <CountDownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountDownContainer>
+        <StartCountDownButton disabled type="submit">
+          <Play size={24} />
+          Começar
+        </StartCountDownButton>
+      </form>
+    </HomeContainer>
+  )
+}
